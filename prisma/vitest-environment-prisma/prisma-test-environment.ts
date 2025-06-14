9620e0e1dddde76ceb7a42a8ed41d8d1bff2b2a8
thiago-guerra <thiago.cesa@hotmail.com>
2025-06-14 17:18:34 -0300

    Implementação de environment dinamico para test vitest prisma

--- prisma/vitest-environment-prisma/prisma-test-environment.ts
+++ prisma/vitest-environment-prisma/prisma-test-environment.ts
@@ -1,13 +1,38 @@
+import { PrismaClient } from "@prisma/client";
+import { execSync } from "node:child_process";
+import { randomUUID } from "node:crypto";
+import { URL } from "node:url";
 import { Environment } from "vitest/environments";
+
+const prisma = new PrismaClient();
+
+function generateDataBaseURL(schema: string) {
+    if (!process.env.DATABASE_URL) {
+        throw new Error('Please provide a DATABASE_URL environment variable.');
+    }
+    const url = new URL(process.env.DATABASE_URL);
+    url.searchParams.set('schema', schema);
+    return url.toString();
+}
 
 export default <Environment> {
     name: 'prisma',
-    transformMode: 'ssr',
+    transformMode: 'web',
     async setup() { 
         console.log('Prisma Test Environment Setup');
+        const schema = randomUUID();
+        const databaseURL = generateDataBaseURL(schema);
+        console.log(' databaseurl', databaseURL);
+        process.env.DATABASE_URL = databaseURL;
+
+        execSync("npx prisma migrate deploy");
+
         return {
             async teardown() {
              console.log('Prisma Test Environment Teardown');
+
+            await  prisma.$executeRawUnsafe(`DROP SCHEMA IF EXISTS "${schema}" CASCADE`);
+            await prisma.$disconnect();
             }
         }
     },
